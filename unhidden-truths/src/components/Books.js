705758bf9695832aef68b113e4711a448cc21099
keenan560 <keenan560@gmail.com>
2020-05-24 20:32:28 -0500

    adding modal

--- unhidden-truths/src/components/Books.js
+++ unhidden-truths/src/components/Books.js
@@ -5,17 +5,30 @@ export default class Books extends React.Component {
 
     constructor(props) {
         super(props);
+        this.getModal = this.getModal.bind(this);
         this.state = { books: books }
+    }
+    getModal() {
+
+        return (
+            <div className='alert'></div>
+        )
+
+
+
     }
 
     renderBooks(books) {
+
         return (
-            <div className='row text-center mx-auto'>
+            <div className='row'>
                 {books.map(book => (
-                    <div className='card mx-1 my-1 animate__animated animate__fadeIn animate__slow'>
-                        <img src={book.image} className='card-img-top shadow-lg' alt={book.title}></img>
-                            {/* <a href={book.image} class="btn book-button btn-sm">Amazon</a>   */}
-                    </div>
+                    <button id='modal-button' type="button" class="btn btn-sm btn-secondary" data-toggle="modal" data-target="#exampleModal">
+                        <div className='card mx-3 my-3 animate__animated animate__fadeIn animate__slow'>
+                            <img src={book.image} className='card-img-top shadow-lg' alt={book.title}></img>
+                        </div>
+                    </button>
+
                 ))}
             </div>
 
@@ -27,7 +40,32 @@ export default class Books extends React.Component {
         return (
             <div className='container'>
                 {this.renderBooks(this.state.books)}
+
+
+
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
         )
     }
 }
--- unhidden-truths/src/components/bookData.js
+++ unhidden-truths/src/components/bookData.js
@@ -1,111 +1,138 @@
 
 const books = [
      {
+        id: 1,
         title: "The Spirit of Python",
         author: "Jentezen Franklin",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKhGCdSImcpy6h2Z29THHXOC9j2ZX27uXTISktJGvyj_7AnE9D-M7gWD9Kf1My5Y5g7rv7MKs&usqp=CAc"
      },
     {
+        id: 2,
         title: "A Divine Revelation of Satan's Deceptions",
         author: "Mary K. Baxter",
         image: "https://images-na.ssl-images-amazon.com/images/I/51-B9u21dPL._SX331_BO1,204,203,200_.jpg"
     }, {
+        id: 3,
         title: "A Divine Revelation Of Spiritual Warfare",
         author: "Mary K. Baxter",
         image: "https://images-na.ssl-images-amazon.com/images/I/51-NSulrFtL._SX329_BO1,204,203,200_.jpg"
     }, {
+        id: 4,
         title: "A Divine Revelation of Angels",
         author: "Mary K. Baxter",
         image: "https://m.media-amazon.com/images/I/51oHJgkX2jL.jpg"
     }, {
+        id: 5,
         title: "Prayers That Rout Demons",
         author: "John Eckhardt",
         image: "https://m.media-amazon.com/images/I/61fZKsM3eWL.jpg"
     }, {
+        id: 6,
         title: "Unmasking the Devil",
         author: "John Ramirez",
         image: "https://m.media-amazon.com/images/I/51ue1V8MvtL.jpg"
     }, {
+        id: 7,
         title: "Prayers That Break Curses",
         author: "John Eckhardt",
         image: "https://m.media-amazon.com/images/I/51YgETnBn9L.jpg"
     }, {
+        id: 8,
         title: "Demons An Eyewitness Account",
         author: "Howard Pitman",
         image: "https://m.media-amazon.com/images/I/519PZZC4XTL.jpg"
     }, {
+        id: 9,
         title: "Prayer Rain",
         author: "Dr D K Olukoya",
         image: "https://m.media-amazon.com/images/I/51pTBKwxZsL.jpg"
     }, {
+        id: 10,
         title: "Curse Breaking: Freedom from the Bondage of Generational Sins ",
         author: "Bob Larson",
         image: "https://i.ebayimg.com/images/g/thgAAOSwWk5cOfXa/s-l640.jpg"
     }, {
+        id: 11,
         title: "Baptize By Blazing Fire",
         author: "Kim Yong-Doo",
         image: "https://m.media-amazon.com/images/I/411M+c1HmxL.jpg"
     }, {
+        id: 12,
         title: "DEFEATING THE DEMON OF POVERTY",
         author: "UEBERT ANGEL",
         image: "https://m.media-amazon.com/images/I/51+HDBZuJzL.jpg"
     }, {
+        id: 13,
         title: "Be Free From Spirit Spouses",
         author: "Zita Grant",
         image: "https://m.media-amazon.com/images/I/51kzKi0hYXL.jpg "
     }, {
+        id: 14,
         title: "Defeating Your Adversary in the Court of Heaven",
         author: "Praying Medic",
         image: "https://m.media-amazon.com/images/I/51IVLm4OvHL.jpg"
     }, {
+        id: 15,
         title: "Operating in the Court of Angels (The Courts of Heaven Book 2)",
         author: "Praying Medic",
         image: "https://m.media-amazon.com/images/I/81eAoVd23aL._AC_UY654_FMwebp_QL65_.jpg"
     }, {
+        id: 16,
         title: "Dealing with Demons",
         author: "Bob Larson",
         image: "https://m.media-amazon.com/images/I/51gIIjOlitL.jpg"
     }, {
+        id: 17,
         title: "Divine Healing Made Simple",
         author: "Praying Medic",
         image: "https://m.media-amazon.com/images/I/51iE9u2dAYL.jpg"
     }, {
+        id: 18,
         title: "Emotional Healing in 3 Easy Steps",
         author: "Praying Medic",
         image: "https://m.media-amazon.com/images/I/51d9oKkXa8L.jpg"
     }, {
+        id: 19,
         title: "Placebo: What is the Church's Dope?",
         author: "Howard Pittman",
         image: "https://m.media-amazon.com/images/I/51pKwO3okSL._SY346_.jpg"
     }, {
+        id: 20,
         title: "Demon-Proofing Prayers",
         author: "Bob Larson",
         image: "https://m.media-amazon.com/images/I/51U0NSEAcfL.jpg"
     }, {
+        id: 21,
         title: "Deliverance and Spiritual Warfare Manual",
         author: "John Eckhardt",
         image: "https://m.media-amazon.com/images/I/512gAasp7uL.jpg"
     }, {
+        id: 22,
         title: "Thou Shall Propser",
         author: "Daniel Lapin",
         image: "https://m.media-amazon.com/images/I/51JDAuusNbL.jpg"
     }, {
+        id: 23,
         title: "23 Minutes In Hell",
         author: "Bill Wiese",
         image: "https://m.media-amazon.com/images/I/516-uYHWQeL._SY346_.jpg"
     }, {
+        id: 24,
         title: "https://m.media-amazon.com/images/I/51djehVSc+L.jpg",
         author: "Robert Henderson",
         image: "https://m.media-amazon.com/images/I/51djehVSc+L.jpg"
     }, {
+        id: 25,
         title: "Prophetic Breakthrough: Decrees that Break Curses and Release Blessings",
         author: "Hakeem Collins",
         image: "https://m.media-amazon.com/images/I/51Crp1gTZfL._SY346_.jpg"
     }, {
+        id: 26,
         title: "The Biblical Marriage: For God or Government?",
         author: "Joshua Paul",
         image: "https://m.media-amazon.com/images/I/51zADajcf0L.jpg"
     }, {
+        id: 27,
         title: "Day Star",
         author: "Howard Pittman",
         image: "https://m.media-amazon.com/images/I/51to4jbx6tL.jpg"
